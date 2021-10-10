50e6fbfe37bc3c60e9511c470dd0d9ef7c4e383f
metterian <dzzy6505@gmail.com>
2021-10-10 20:45:01 +0900

    fix: rename sim_socre, accept_score

--- web.py
+++ web.py
@@ -1,7 +1,6 @@
 import streamlit as st
-from typing import List
-
 from streamlit.state.session_state import SessionState
+from typing import List
 from upload import logo
 from truecase import get_true_case
 import random
@@ -9,28 +8,31 @@ import requests
 
 from urllib.parse import urljoin
 
-URL = 'http://nlplab.iptime.org:47079/'
+URL = "http://nlplab.iptime.org:47079/"
 
 
-def right_align(text, bold = False):
+def right_align(text, bold=False):
     return f"<p style='text-align: right;'>{text}</p>"
+
 
 def bold(text):
     return f"<p style='text-align: right;'><b>{text}</b></p>"
 
+
 def display_dialogue():
-    '''display between Human and Chatbot conversation'''
+    """display between Human and Chatbot conversation"""
     if not st.session_state.chatbot:
         return
     for human, chatbot in zip(st.session_state.human, st.session_state.chatbot):
-        st.markdown(right_align(f'You: {human}'), unsafe_allow_html=True)
-        st.write(f'Bot: {get_true_case(chatbot)}')
+        st.markdown(right_align(f"You: {human}"), unsafe_allow_html=True)
+        st.write(f"Bot: {get_true_case(chatbot)}")
+
 
 # Initialization
-if 'chatbot' not in st.session_state:
-    st.session_state['human'] = []
-    st.session_state['chatbot'] = []
-    requests.get(urljoin('history/clear', URL))
+if "chatbot" not in st.session_state:
+    st.session_state["human"] = []
+    st.session_state["chatbot"] = []
+    requests.get(urljoin("history/clear", URL))
 
 
 # title
@@ -38,13 +40,13 @@ title_container = st.container()
 col1, col2 = st.columns([1, 20])
 with title_container:
     with col1:
-        st.image('/Users/seungjun/PyProject/peep-talk-web-demo/logo.png', width=64)
+        st.image("/Users/seungjun/PyProject/peep-talk-web-demo/logo.png", width=64)
     with col2:
-        st.title('PEEP-Talk')
+        st.title("PEEP-Talk")
 
 
 # sub title - Personality
-personality = requests.get(urljoin(URL, 'personality')).json()
+personality = requests.get(urljoin(URL, "personality")).json()
 personality = map(lambda string: string.capitalize(), personality)
 st.subheader("Personality")
 st.text("\n".join(personality))
@@ -54,37 +56,51 @@ st.text("\n".join(personality))
 st.subheader("Context Detector")
 col1, col2 = st.columns(2)
 
-def diplay_cd(sim_score: int, lang_score: int):
+
+def diplay_cd(sim_score: int, accept_score: int):
     with col1:
         slider = st.slider(
-            label='Context Similarity', min_value=0,
-            max_value=100, value=sim_score, key='sim')
+            label="Context Similarity",
+            min_value=0,
+            max_value=100,
+            value=sim_score,
+            key="sim",
+        )
 
     with col2:
         slider = st.slider(
-            label='Linguistic Acceptability', min_value=0,
-            max_value=100, value=lang_score, key='error')
+            label="Linguistic Acceptability",
+            min_value=0,
+            max_value=100,
+            value=accept_score,
+            key="error",
+        )
+
 
 # divide section
-st.markdown('---')
+st.markdown("---")
 
 dialogue_container = st.container()
 
 # input user text
-with st.form(key='user_form', clear_on_submit=True):
-    user_input = st.text_input(label='Type a message')
-    submit_button = st.form_submit_button(label='Submit')
+with st.form(key="user_form", clear_on_submit=True):
+    user_input = st.text_input(label="Type a message")
+    submit_button = st.form_submit_button(label="Submit")
 
     if submit_button:
-        st.session_state['human'].append(user_input)
-        response = requests.post(urljoin(URL, 'message'), json = {'user_input': user_input}).json()
-        st.session_state['chatbot'].append(response['message'])
+        st.session_state["human"].append(user_input)
+        response = requests.post(
+            urljoin(URL, "message"), json={"user_input": user_input}
+        ).json()
+        st.session_state["chatbot"].append(response["message"])
 
 # messsage box
 if submit_button:
-    with dialogue_container.form(key='bot_form'):
+    with dialogue_container.form(key="bot_form"):
         # st.write('Bot')
         display_dialogue()
-        print(response)
-        reset_button = st.form_submit_button(label='', on_click=diplay_cd(int(response['similarity']), int(response['acceptability'])))
-
+        sim_score = int(response["similarity"])
+        accept_score = int(response["acceptability"])
+        reset_button = st.form_submit_button(
+            label="", on_click=diplay_cd(sim_score, accept_score)
+        )
